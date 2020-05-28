6096ccbbf217121dd32f55fabd22ca0453248598
vienuit <vienbinhthanh@gmail.com>
2020-05-28 19:36:03 +0700

    Updates

---
+++ precache-manifest.5eb2ae4a097f006c84c5ff69997e6022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bf2701b2d6a7da2382317bd1505fdc",
+    "url": "/note-react/index.html"
+  },
+  {
+    "revision": "99979c9453333aba4769",
+    "url": "/note-react/static/css/main.29f720eb.chunk.css"
+  },
+  {
+    "revision": "b4dc24cddfd2816a89b1",
+    "url": "/note-react/static/js/2.5aa3fd7b.chunk.js"
+  },
+  {
+    "revision": "01085fbccea441e37c81e3bac77ce2cc",
+    "url": "/note-react/static/js/2.5aa3fd7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99979c9453333aba4769",
+    "url": "/note-react/static/js/main.b3f80ed2.chunk.js"
+  },
+  {
+    "revision": "721bae21cdfc130d2294",
+    "url": "/note-react/static/js/runtime-main.8bd2353e.js"
+  }
+]);
